174909ad837c35018a2e2b9684fb3ef7db2a0c06
RyanMolina <ryangabrielpmolina@gmail.com>
2018-03-04 18:45:33 +0800

    separate dataset_name and model_name

--- train.py
+++ train.py
@@ -15,82 +15,89 @@ except SystemError:
 def main():
     """Contains all of the work that needs to be done."""
     args = parse_args()
+    dataset_name = args.dataset_name
     model_name = args.model_name
+    corpus_name = args.corpus_name
 
     FILE_PATH = os.path.dirname(__file__)
-    training_path = os.path.join(FILE_PATH, 'training', 'data', 'dataset')
-    os.makedirs(os.path.join(training_path, model_name), exist_ok=True)
+    TRAIN_PATH = os.path.join(FILE_PATH, 'training')
+    MODEL_PATH = os.path.join(TRAIN_PATH, 'model', model_name)
+    DATASET_PATH = os.path.join(TRAIN_PATH, 'data', 'dataset', dataset_name)
+    CORPUS_PATH = os.path.join(TRAIN_PATH, 'data', 'corpus', corpus_name)
 
-    if args.corpus:
-        print("> Generating dataset from the corpus file...")
-        corpus_path = os.path.join(FILE_PATH, 'training', 'data', 'corpus')
-        articles_path = os.path.join(corpus_path, args.corpus)
-        noisify_dataset.run(articles_path,
-                            os.path.join(training_path,
-                                         model_name,
-                                         'dataset.dec'),
-                            char_level_emb=args.char_emb,
-                            augment_data=args.augment_data,
-                            shuffle=args.shuffle,
-                            max_seq_len=args.max_seq_len)
 
-    if args.split_dataset:
-        print("> Splitting the dataset to train/dev/test set...")
-        split_dataset.split(
-            os.path.join(
-                training_path, model_name,
-                'dataset.enc'),
-            os.path.join(training_path, model_name),
-            'enc', test_size=500, dev_size=500)
+    if args.corpus_name:
+        if not os.path.exists(DATASET_PATH):
+            print("> Generating dataset from the corpus file...") 
+            os.makedirs(DATASET_PATH, exist_ok=True)
+            noisify_dataset.run(CORPUS_PATH,
+                                os.path.join(MODEL_PATH,
+                                             'dataset.dec'),
+                                char_level_emb=args.char_emb,
+                                augment_data=args.augment_data,
+                                shuffle=args.shuffle,
+                                max_seq_len=args.max_seq_len)
 
-        split_dataset.split(
-            os.path.join(
-                training_path, model_name,
-                'dataset.dec'),
-            os.path.join(training_path, model_name),
-            'dec', test_size=500, dev_size=500)
+            if args.split_dataset:
+                print("> Splitting the dataset to train/dev/test set...")
+                split_dataset.split(
+                    os.path.join(
+                        training_path, model_name,
+                        'dataset.enc'),
+                    os.path.join(training_path, model_name),
+                    'enc', test_size=500, dev_size=500)
 
-    if args.generate_vocab:
-        print("> Generating the vocab files...")
-        generate_vocab.get_vocab(
-            os.path.join(training_path, model_name, 'train.enc'),
-            max_vocab_size=20000)
+                split_dataset.split(
+                    os.path.join(
+                        training_path, model_name,
+                        'dataset.dec'),
+                    os.path.join(training_path, model_name),
+                    'dec', test_size=500, dev_size=500)
 
-        generate_vocab.get_vocab(
-            os.path.join(training_path, model_name, 'train.dec'),
-            max_vocab_size=20000)
+            if args.generate_vocab:
+                print("> Generating the vocab files...")
+                generate_vocab.get_vocab(
+                    os.path.join(training_path, model_name, 'train.enc'),
+                    max_vocab_size=20000)
+
+                generate_vocab.get_vocab(
+                    os.path.join(training_path, model_name, 'train.dec'),
+                    max_vocab_size=20000)
+        else:
+            print('> Dataset already exists')
 
     if args.train:
         print("> Start trainining...")
         print(FILE_PATH)
-        data_dir = os.path.join(FILE_PATH, 'training', 'data', 'dataset', model_name)
-        model_dir = os.path.join(FILE_PATH, 'training', 'model', model_name)
-        os.makedirs(model_dir, exist_ok=True)
-        hparams_file = os.path.join(FILE_PATH, 'hparams.json')
-        noisify_dataset_file = os.path.abspath(noisify_dataset.__file__)
 
-        if not os.path.exists(os.path.join(model_dir, 'hparams.json')):
+        os.makedirs(MODEL_PATH, exist_ok=True)
+        hparams_file = os.path.join(FILE_PATH, 'hparams.json')
+
+        noisify_dataset_file = os.path.abspath(noisify_dataset.__file__)
+        if not os.path.exists(os.path.join(MODEL_PATH, 'hparams.json')):
             print("> Backing up hparams.json")
-            shutil.copy(hparams_file, os.path.join(model_dir))
+            shutil.copy(hparams_file, MODEL_PATH)
         else:
             print('> Failed to back up: ', hparams_file)
 
-        if not os.path.exists(os.path.join(data_dir, 'noisify_dataset.py')):
+        if not os.path.exists(os.path.join(DATASET_PATH, 'noisify_dataset.py')):
             print("> Backing up noisify_dataset.py")
             shutil.copy(noisify_dataset_file,
-                        os.path.join(data_dir, 'noisify_dataset.py.bak'))
+                        os.path.join(DATASET_PATH, 'noisify_dataset.py.bak'))
         else:
             print('> Failed to back up: ', noisify_dataset_file)
 
         hparams = utils.load_hparams(
-            os.path.join(model_dir, hparams_file))
+            os.path.join(MODEL_PATH, hparams_file))
+
         normalizer_trainer = trainer.Trainer(
-            data_dir=data_dir, model_dir=model_dir, hparams=hparams)
+            data_dir=DATASET_PATH, model_dir=MODEL_PATH, hparams=hparams)
         normalizer_trainer.train()
 
 
 def parse_args():
     """Parse the arguments needed before running.
+
 
     Returns:
         args : contains the source text, model name, and if
@@ -101,46 +108,35 @@ def parse_args():
 
     parser.register("type", "bool", lambda v: v.lower() == "true")
 
-    parser.add_argument('model_name', default='model', type=str,
+    parser.add_argument('--model_name', default='model', type=str,
                         help="Unique name for each model you train.")
 
-    parser.add_argument('--corpus', type=str, default=None,
+    parser.add_argument('--dataset_name', default='dataset', type=str,
+                        help="Unique name of dataset you use for training")
+
+    parser.add_argument('--corpus_name', type=str, default=None,
                         help="Generate parallel noisy text")
 
-    parser.add_argument('--split_dataset',
-                        type="bool", nargs="?", const=False,
-                        default=False,
-                        help="""Split dataset to train/dev/test""")
-
-    parser.add_argument('--generate_vocab',
-                        type="bool", nargs="?", const=False,
-                        default=False,
-                        help="""Generate the vocab file from train file""")
-
     parser.add_argument('--train',
-                        type="bool", nargs="?", const=False,
-                        default=False,
+                        type="bool", nargs="?", const=True,
+                        default=True,
                         help="Start/Resume train")
-    
-    parser.add_argument('--noisify_mode',
-                        default='random', type=str,
-                        help="Noisification mode [random, misspell, remove_vowels, etc]")
 
     parser.add_argument('--char_emb',
-                        type="bool", nargs="?", const=False,
-                        default=False,
+                        type="bool", nargs="?", const=True,
+                        default=True,
                         help="""Embedding type
                         True for char-level, False for word-level""")
 
     parser.add_argument('--augment_data',
-                        type="bool", nargs="?", const=False,
-                        default=False,
+                        type="bool", nargs="?", const=True,
+                        default=True,
                         help="""Augment data by adding the dataset vocabulary
                         and the n-gram from unigram to 6-gram""")
 
     parser.add_argument('--shuffle',
-                        type="bool", nargs="?", const=False,
-                        default=False,
+                        type="bool", nargs="?", const=True,
+                        default=True,
                         help="""Shuffle the dataset""")
 
     parser.add_argument('--max_seq_len', default=140, type=int,
